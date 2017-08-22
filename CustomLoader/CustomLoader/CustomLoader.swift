a578ec399efcc874525123f3e7a589f289b3222b
Pardeep <pkc456@gmail.com>
2017-08-22 22:43:58 +0530

    Added loader images

--- CustomLoader/CustomLoader/CustomLoader.swift
+++ CustomLoader/CustomLoader/CustomLoader.swift
@@ -17,13 +17,25 @@ class CustomLoader: UIView {
         
         
         animatedImageView.animationImages = [
-            UIImage(named:"7")!,
+//            UIImage(named:"7")!,
             UIImage(named:"1")!,
             UIImage(named:"2")!,
             UIImage(named:"3")!,
             UIImage(named:"4")!,
             UIImage(named:"5")!,
             UIImage(named:"6")!,
+            UIImage(named:"7")!,
+            UIImage(named:"8")!,
+            UIImage(named:"9")!,
+            UIImage(named:"10")!,
+            UIImage(named:"11")!,
+            UIImage(named:"12")!,
+            UIImage(named:"13")!,
+            UIImage(named:"14")!,
+            UIImage(named:"15")!,
+            UIImage(named:"16")!,
+            UIImage(named:"17")!,
+            UIImage(named:"18")!,
         ]
         
         animatedImageView.animationDuration = 1.0
